3e2189de8a92f64cd82223767b4b25fe5f0ceaf6
Jaroslav Tesař <jarabt@seznam.cz>
2018-12-28 12:07:03 +0100

    first commit

---
+++ E2-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+  printf ("1. In C, lowercase letters are significant.\n");
+  printf ("2. main() is where program execution begins.\n");
+  printf ("3. Opening and closing braces enclose program statements in a routine.\n");
+  printf ("4. All program statement must be terminated by a semicolon.\n");
+  
+  return 0;
+}
